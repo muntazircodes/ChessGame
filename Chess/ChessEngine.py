9fa4f16199f46a1a897dfe029a238dbf30caca53
Muntazir Manan Dar <muntazircodes@gmail.com>
2023-10-05 22:40:17 +0530

    The file is just updated to draw board

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -1,6 +1,10 @@
 class GameState:
     def __init__(self):
 
+
+        # THIS IS THE 8 * 8 2D LIST WHICH HAS 2 CHARACTERS. 
+        # THE FIRST CHARACTER REPRENSTS THE COLOR AND THE SECOND CHARACTER REPRESENTS THE PIECE.
+
         self.board = [
                 ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
                 ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
@@ -12,5 +16,7 @@ class GameState:
                 ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
             ]
         
+        # THIS WILL HELP TO TOGGLE AND CHANGING THE TURNS AFTER WE WILL CREATE A FUNCTION LATER ON
         self.whiteToMove = True
+        # THIS WILL KEEP THE TRACK OF MOVES
         self.moveLog = []
