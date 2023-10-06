69ce0c8b39ac7f070679b19dea86488b8fb1f78a
Muntazir Manan Dar <muntazircodes@gmail.com>
2023-10-06 17:24:49 +0530

    The file is updated to change the board state on click and right now with out any restrictions and rules

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -1,4 +1,4 @@
-class GameState:
+class GameState():
     def __init__(self):
 
 
@@ -20,3 +20,41 @@ class GameState:
         self.whiteToMove = True
         # THIS WILL KEEP THE TRACK OF MOVES
         self.moveLog = []
+
+    def makeMove(self, move):
+            self.board[move.startRow][move.startCol] = "__"
+            self.board[move.endRow][move.endCol] = move.pieceMoved
+            self.moveLog.append(move)
+
+            self.whiteToMove = not self.whiteToMove
+
+class Move():
+
+    # MAPS OF KEY AND VALUES TO CREATE NOTATION
+
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                    "5": 3, "6": 2, "7": 1, "8": 0}  # THIS WILL HELP US TO LABEL THE ROWS ON THE BASIS OF THEIR INDEXES
+
+    rowsToRanks = {v:k for k, v in ranksToRows.items()}
+
+    filesToCols = {"a" :0, "b": 1, "c": 2, "d": 3,
+                   "e": 4, "f": 5, "g": 6, "h": 7}   # THIS WILL HELP US TO LABEL THE COLS ON THE BASIS OF THEIR INDEXES
+    
+    colsToFiles = {v:k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow  = endSq[0]
+        self.endCol = endSq[1]
+
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+
+        # WE ADD THIS LINE TO GET THE REAL CHESS NOTATION
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+    
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
